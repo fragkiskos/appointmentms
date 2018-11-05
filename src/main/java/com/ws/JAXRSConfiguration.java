008200d9cd25390415312bf059f8047e9d324d68
alefas88 <alefas88@gmail.com>
2018-11-05 22:58:33 +0200

    test commit

--- src/main/java/com/ws/JAXRSConfiguration.java
+++ src/main/java/com/ws/JAXRSConfiguration.java
@@ -6,7 +6,7 @@ import javax.ws.rs.core.Application;
 /**
  * Configures a JAX-RS endpoint. Delete this class, if you are not exposing
  * JAX-RS resources in your application.
- *
+ *lk
  */
 @ApplicationPath("resources")
 public class JAXRSConfiguration extends Application {
